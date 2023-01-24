fa62beaa9ddd1a01d0c98b5f1ef01d92c6c42754
Diego <Diegoalizo75@gmail.com>
2023-01-24 13:15:15 -0500

    VIP Reminder

---
+++ src/VIPreminder.java
@@ -0,0 +1,5 @@
+public class VIPreminder {
+    public static void main(String[] args) {
+        System.out.println("Reminder to reestablish connection with the remote repository if when reopening console");
+    }
+}
